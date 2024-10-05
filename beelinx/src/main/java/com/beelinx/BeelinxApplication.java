caedf0f4fde220e7eba45f677b90eb25701bb263
Princekumartest <kumartestprince@gmail.com>
2024-10-05 15:29:36 +0530

    Add files via upload

---
+++ beelinx/src/main/java/com/beelinx/BeelinxApplication.java
@@ -0,0 +1,13 @@
+package com.beelinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeelinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeelinxApplication.class, args);
+	}
+
+}
---
+++ beelinx/src/test/java/com/beelinx/BeelinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beelinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeelinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
